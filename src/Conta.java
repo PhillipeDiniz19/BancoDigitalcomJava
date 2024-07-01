37d03a9c41703ef1ec78eb91c9579c3ca4512845
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-07-01 16:16:09 -0300

    finalizando bando digital

--- src/Conta.java
+++ src/Conta.java
@@ -1,12 +1,35 @@
 
-public class Conta {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public abstract class Conta implements IConta {
 
-    public void sacar(){}
-    public void depositar(){}
-    public void transferir(){}
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
 
     public int getAgencia() {
         return agencia;
@@ -18,5 +41,12 @@ public class Conta {
 
     public double getSaldo() {
         return saldo;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 }
