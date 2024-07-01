0341c2112081236832da4cbc6d454a7f8f82b6a7
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-07-01 16:01:25 -0300

    aplicando a heranca

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,14 +1,3 @@
-public class ContaPoupanca {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaPoupanca extends Conta {
 
-    public ContaPoupanca(int numero, int agencia) {
-        this.numero = numero;
-        this.agencia = agencia;
-    }
-
-    public void sacar(){}
-    public void depositar(){}
-    public void transferir(){}
 }
