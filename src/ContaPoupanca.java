53e0a2eea3fcbed84026a2a18bf6c908eb3a8b8b
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-07-01 15:56:30 -0300

    gerando getter and set java

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,5 +1,14 @@
 public class ContaPoupanca {
-    public int agencia;
-    public int numero;
-    public double saldo;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public ContaPoupanca(int numero, int agencia) {
+        this.numero = numero;
+        this.agencia = agencia;
+    }
+
+    public void sacar(){}
+    public void depositar(){}
+    public void transferir(){}
 }
